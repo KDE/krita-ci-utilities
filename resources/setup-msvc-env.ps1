972375a4007e8178bbe1e9b4d5c3bcea42d28f98
Ben Cooksley <bcooksley@kde.org>
2022-01-02 22:30:23 +1300

    Minor syntax fixes to resolve escaping issues

--- resources/setup-msvc-env.ps1
+++ resources/setup-msvc-env.ps1
@@ -1,4 +1,4 @@
-$VCVars64 = 'C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Auxiliary/Build/vcvars64.bat'
+$VCVars64 = "`"C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Auxiliary/Build/vcvars64.bat`""
 
 cmd /c "$VCVars64 > nul 2>&1 && set" | . { process {
     if ($_ -match '^([^=]+)=(.*)') {
