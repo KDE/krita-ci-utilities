092bd47b2e16e333daf62c2b494c11a16a7afdbf
Ben Cooksley <bcooksley@kde.org>
2023-01-22 08:51:42 +1300

    Revert "Cleanup Plasma/5.26 CI support"
    People are still messing around with this.
    
    This reverts commit ee9fad5b82f1f184e46d07658c22ca34ee240df8.

--- package-registry-cleanup.py
+++ package-registry-cleanup.py
@@ -45,7 +45,7 @@ for package in remoteRegistry.packages.list( as_list=False ):
     }
 
     # Is this a stale branch we can let go of?
-    if branch in ['release-21.08', 'release-21.12', 'release-22.04', 'release-22.08', 'Plasma-5.26']:
+    if branch in ['release-21.08', 'release-21.12', 'release-22.04', 'release-22.08']:
         # Then mark it for removal
         packagesToRemove.append( packageData['package'] )
         continue
