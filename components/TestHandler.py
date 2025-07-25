9645b0cca3ee444022d192a5991b98edf51c1df2
Ben Cooksley <bcooksley@kde.org>
2025-07-26 07:46:55 +1200

    Be more violent in ensuring Xvfb and co are no longer around

--- components/TestHandler.py
+++ components/TestHandler.py
@@ -209,7 +209,7 @@ def run( projectConfig, sourcesPath, buildPath, installPath, buildEnvironment ):
         subprocess.call("taskkill /f /T /im kioslave5.exe", shell=True)
         subprocess.call("taskkill /f /T /im vctip.exe", shell=True)
 
-    if sys.platform == 'freebsd12' or sys.platform == 'freebsd13':
+    if sys.platform != 'win32':
         subprocess.call("killall -9 dbus-daemon kded5 kioslave klauncher kdeinit5 kiod openbox Xvfb lldb", shell=True)
 
     # All done!
