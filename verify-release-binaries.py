8aa20dbbdd93f413f9f0b02ce56a5e4588b7cd01
Dmitry Kazakov <dimula73@gmail.com>
2026-06-26 14:33:51 +0200

    Fix verify-release-binaries to actually verify signatures

--- verify-release-binaries.py
+++ verify-release-binaries.py
@@ -10,20 +10,12 @@ import sys
 import fnmatch
 from components import PlatformFlavor, CommonUtils
 
-has_pefile = False
-
-try:
-    import pefile
-    has_pefile = True
-except:
-    pass
-
 parser = argparse.ArgumentParser(description=f'Searches for binary files with DEBUG section present.')
 parser.add_argument('-d', '--directory', default=None, help='Directory to search (default: current directory)')
 parser.add_argument('-f', '--file', default=None, help='Executable file to check')
 parser.add_argument('-p', '--platform', default=None, help='Platform the package is built for (will use KDECI_TARGET_PLATFORM if missing)')
-if has_pefile:
-    parser.add_argument('-s', '--signature', action='store_true', default=None, help='Verify that all modules in the directory has signature entry (no signature validation happens)')
+parser.add_argument('-s', '--signature-entry', action='store_true', help='Verify that all modules in the directory has signature entry (no signature validation happens)')
+parser.add_argument('-v', '--verify-signature', action='store_true', help='Verify that all the modules have a digital signature **and** that the signature is valid')
 args = parser.parse_args()
 
 if args.directory is None and args.file is None:
@@ -38,16 +30,13 @@ if args.platform is None:
     else:
         raise Exception("No target platform is provided")
 
-if has_pefile and args.signature is None:
-    if "KDECI_SIGN_BINARIES" in os.environ:
-        args.signature = CommonUtils.boolFromEnv(os.environ["KDECI_SIGN_BINARIES"])
-        print(f"INFO: signature verification is set by KDECI_SIGN_BINARIES: {args.signature}")
-    else:
-        args.signature = False
+if "KDECI_SIGN_BINARIES" in os.environ:
+    args.verify_signature = CommonUtils.boolFromEnv(os.environ["KDECI_SIGN_BINARIES"])
+    print(f"INFO: signature verification is set by KDECI_SIGN_BINARIES: {args.verify_signature}")
 
 platform = PlatformFlavor.PlatformFlavor(args.platform)
 
-if has_pefile and args.signature and not platform.matches(['Windows']):
+if (args.signature_entry or args.verify_signature)  and not platform.matches(['Windows']):
     warnings.warn(f"WARNING: signature verification for a non-Windows platform is not supported")
 
 glob_patterns = CommonUtils.globPatternsForBinaries(platform)
@@ -67,11 +56,27 @@ def has_debug_section(objdumpOutput):
     return False
 
 def has_certificate_entry(filePath):
+    import pefile
     # NOTE: we do **not** verify the signature itself here,
     # we just check if the entry is present in the PE-structure
-    pe = pefile.PE(filePath, fast_load=True)
-    address = pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_SECURITY"]
-    return pe.OPTIONAL_HEADER.DATA_DIRECTORY[address].Size > 0
+    with pefile.PE(filePath, fast_load=True) as pe:
+        address = pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_SECURITY"]
+        return pe.OPTIONAL_HEADER.DATA_DIRECTORY[address].Size > 0
+
+def verify_pe_signature(filePath):
+    from signify.authenticode import AuthenticodeFile
+    from signify.authenticode import AuthenticodeVerificationResult
+    with open(filePath, "rb") as f:
+        try:
+            pe_file = AuthenticodeFile.from_stream(f)
+            status, err = pe_file.explain_verify()
+            if status == AuthenticodeVerificationResult.OK:
+                return True
+            else:
+                warnings.warn(f"ERROR: couldn't verify signature for {filePath}: {err}")
+
+        except Exception as e:
+            warnings.warn(f"ERROR: couldn't process file {filePath}: {e}")
 
 OBJDUMP = CommonUtils.detectObjdump()
 if not OBJDUMP:
@@ -85,10 +90,13 @@ def verify_one_file(file):
     global anyLibsWithDebugFound
     global anyUnsignedFound
     try:
-        if has_pefile and args.signature:
+        if args.signature_entry:
             if not has_certificate_entry(file):
               anyUnsignedFound = True
               print(f"File is not signed: {file}")
+        if args.verify_signature:
+            if not verify_pe_signature(file):
+              anyUnsignedFound = True
         result = subprocess.run([OBJDUMP, '-h', file], capture_output=True, text=True, check=True)
         if has_debug_section(result.stdout):
             anyLibsWithDebugFound = True
